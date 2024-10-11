7704f10ebd190b192eaa6f9b4797739128bb274b
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-11 17:24:17 +0600

    Displayed Feed container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Feed from "./Containers/Feed/Feed.tsx";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Feed></Feed>
+    </>
+  );
 };
 
 export default App;
